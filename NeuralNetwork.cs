5c4266e2e678108330fc604d9bf5d28dff280fbc
BenjaminPV <BenjaminPV@users.noreply.github.com>
2023-06-08 01:23:25 +0200

    Update NeuralNetwork.cs

--- NeuralNetwork.cs
+++ NeuralNetwork.cs
@@ -1,21 +1,38 @@
 
+// Should be helpful
+class Neuron
+{
+    // Liste of Weights (one for each Neuron in the next Layer) 
+    
+    // Methods
+    // 1. Constructor
+    // 2. Receiving input
+    // 3. Computing output to one Neuron
+    // 4. Sending output to all the Neurons in the following Layer
+}
+
+// Helpful?
+class Layer 
+{
+    // List of Neurons
+}
 
 class NeuralNetwork
     {
-        //Initilaizing variables
-        int layers;
+        //Initializing variables
+        int numberOfLayers;
 
-        float[][] neurons;    //two demensional matrix
-        float[][][] weigths; //three demesnional matrix
+        float[][] neurons;    //two dimensional matrix
+        float[][][] weigths; //three dimensional matrix
         
 
 
         //Constructer 
-        public NeuralNetwork(int layers, int[] layerSize)
+        public NeuralNetwork(int numberOfLayers, int[] layerSize) 
         {
-            this.layers = layers;
-            neurons = new float[this.layers][];       // Adding all the Layers
-            weigths = new float[this.layers - 1][][];//Strucuring the weights matrix (Input Layer doesnt have incomming weights)
+            this.numberOfLayers = numberOfLayers;
+            neurons = new float[this.numberOfLayers][];       // Adding all the Layers
+            weigths = new float[this.numberOfLayers - 1][][];//Strucuring the weights matrix (Input Layer doesnt have incomming weights)
 
             for (int i = 0;  i < this.layers; i++)
             {
