28cc5ccdd1cc8e18516310cbf3d469456d40af2c
OL1Y1ER <134446761+OL1Y1ER@users.noreply.github.com>
2023-06-08 23:36:13 +0200

    Update NeuralNetwork.cs

--- NeuralNetwork.cs
+++ NeuralNetwork.cs
@@ -1,35 +1,54 @@
+
 class NeuralNetwork
 {
-    // Initializing variables
+    //Initilaizing variables
     int layers;
 
-    public float[][] neurons;    // two-dimensional matrix
-    public float[][][] weights;  // three-dimensional matrix
-    public int[] biases;
+    public float[][] neurons;    //two demensional matrix
+    public float[][][] weigths; //three demesnional matrix
+    public int[] bais;         // array for all bais 
 
-    // Constructor 
+
+
+    //Constructer Initializes the Neural Network
     public NeuralNetwork(int layers, int[] layerSize)
     {
         this.layers = layers;
         neurons = new float[this.layers][];       // Adding all the Layers
-        weights = new float[this.layers - 1][][]; // Structuring the weights matrix (Input Layer doesn't have incoming weights)
+        weigths = new float[this.layers - 1][][];//Strucuring the weights matrix (Input Layer doesnt have incomming weights)
         int basisLength = 0;
 
         for (int i = 0; i < this.layers; i++)
         {
-            neurons[i] = new float[layerSize[i]]; // Adding all the Neurons in each layer
+            neurons[i] = new float[layerSize[i]]; //  Adding all the Neurons in each layer
             basisLength += layerSize[i];
 
             if (i != 0)
             {
-                weights[i - 1] = new float[layerSize[i]][];
+                weigths[i - 1] = new float[layerSize[i]][];
                 for (int j = 0; j < layerSize[i]; j++)
                 {
-                    weights[i - 1][j] = new float[layerSize[i - 1]]; // Adding all the weights to all the neurons
+                    weigths[i - 1][j] = new float[layerSize[i - 1]]; // Adding all the weigths to all the neurons
+
+
                 }
             }
         }
 
-        biases = new int[basisLength - layerSize[0]];
+        bais = new int[basisLength - layerSize[0]];
+
     }
+
+
+    float[] Prediction(float[] input)
+    {
+
+
+        return null;
+    }
+
+
+
+
+
 }
