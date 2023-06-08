af6e2655a4f098184b6052243965ccd7c75e849f
OL1Y1ER <134446761+OL1Y1ER@users.noreply.github.com>
2023-06-08 18:29:35 +0200

    Corect spelling errors

--- NeuralNetwork.cs
+++ NeuralNetwork.cs
@@ -1,45 +1,35 @@
 class NeuralNetwork
 {
-    //Initilaizing variables
+    // Initializing variables
     int layers;
 
-    public float[][] neurons;    //two demensional matrix
-    public float[][][] weigths; //three demesnional matrix
-    public int[] bais;
+    public float[][] neurons;    // two-dimensional matrix
+    public float[][][] weights;  // three-dimensional matrix
+    public int[] biases;
 
-
-    //Constructer 
+    // Constructor 
     public NeuralNetwork(int layers, int[] layerSize)
     {
         this.layers = layers;
         neurons = new float[this.layers][];       // Adding all the Layers
-        weigths = new float[this.layers - 1][][];//Strucuring the weights matrix (Input Layer doesnt have incomming weights)
+        weights = new float[this.layers - 1][][]; // Structuring the weights matrix (Input Layer doesn't have incoming weights)
         int basisLength = 0;
 
         for (int i = 0; i < this.layers; i++)
         {
-            neurons[i] = new float[layerSize[i]]; //  Adding all the Neurons in each layer
+            neurons[i] = new float[layerSize[i]]; // Adding all the Neurons in each layer
             basisLength += layerSize[i];
 
             if (i != 0)
             {
-                weigths[i - 1] = new float[layerSize[i]][];
+                weights[i - 1] = new float[layerSize[i]][];
                 for (int j = 0; j < layerSize[i]; j++)
                 {
-                    weigths[i - 1][j] = new float[layerSize[i - 1]]; // Adding all the weigths to all the neurons
-
-
+                    weights[i - 1][j] = new float[layerSize[i - 1]]; // Adding all the weights to all the neurons
                 }
             }
         }
 
-        bais = new int[basisLength - layerSize[0]];
-
-
-
+        biases = new int[basisLength - layerSize[0]];
     }
-
-
-
-
 }
