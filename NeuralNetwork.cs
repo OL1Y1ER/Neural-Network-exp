367e9cf0ae156fdb59615e5dca0a46b04b0c9648
OL1Y1ER <134446761+OL1Y1ER@users.noreply.github.com>
2023-06-12 22:17:01 +0200

    Spelling errors fixed

--- NeuralNetwork.cs
+++ NeuralNetwork.cs
@@ -1,94 +1,77 @@
 class NeuralNetwork
 {
-    //Initilaizing variables
+    // Initializing variables
     int layers;
+    float[][] neurons;    // Two-dimensional matrix
+    float[][][] weights; // Three-dimensional matrix
+    int[] bias;         // Array for all biases 
 
-    float[][] neurons;    //two demensional matrix
-    float[][][] weigths; //three demesnional matrix
-    int[] bais;         // array for all bais 
-
-
-
-    //Constructer Initializes the Neural Network
+    // Constructor initializes the Neural Network
     public NeuralNetwork(int layers, int[] layerSize)
     {
         this.layers = layers;
         neurons = new float[this.layers][];       // Adding all the Layers
-        weigths = new float[this.layers - 1][][];//Strucuring the weights matrix (Input Layer doesnt have incomming weights)
-        int basisLength = 0;
+        weights = new float[this.layers - 1][][]; // Structuring the weights matrix (Input Layer doesn't have incoming weights)
+        int biasLength = 0;
 
         for (int i = 0; i < this.layers; i++)
         {
-            neurons[i] = new float[layerSize[i]]; //  Adding all the Neurons in each layer
-            basisLength += layerSize[i];
+            neurons[i] = new float[layerSize[i]]; // Adding all the Neurons in each layer
+            biasLength += layerSize[i];
 
             if (i != 0)
             {
-                weigths[i - 1] = new float[layerSize[i]][];
+                weights[i - 1] = new float[layerSize[i]][];
                 for (int j = 0; j < layerSize[i]; j++)
                 {
-                    weigths[i - 1][j] = new float[layerSize[i - 1]]; // Adding all the weigths to all the neurons
-
-
+                    weights[i - 1][j] = new float[layerSize[i - 1]]; // Adding all the weights to all the neurons
                 }
             }
         }
 
-        bais = new int[basisLength - layerSize[0]];
-
+        bias = new int[biasLength - layerSize[0]];
     }
 
-
-
-
     public float[] Prediction(float[] inputs)
-
     {
-        int baisIndex = 0;
-
-        neurons[0] = inputs;//adding inputs to InputLayer
+        int biasIndex = 0;
+        neurons[0] = inputs; // Adding inputs to the input layer
 
         for (int i = 1; i < layers; i++)
         {
             for (int j = 0; j < neurons[i].Length; j++)
             {
-                //neurons[i][j] = 0; // Initialize neuron value
-                for (int k = 0; k < weigths[i - 1][j].Length; k++)
+                // neurons[i][j] = 0; // Initialize neuron value
+                for (int k = 0; k < weights[i - 1][j].Length; k++)
                 {
-                    neurons[i][j] += neurons[i - 1][k] * weigths[i - 1][j][k];
-
+                    neurons[i][j] += neurons[i - 1][k] * weights[i - 1][j][k];
                 }
-                neurons[i][j] += bais[baisIndex];
-                baisIndex++;
+                neurons[i][j] += bias[biasIndex];
+                biasIndex++;
             }
         }
 
         return neurons[neurons.Length - 1];
     }
 
-    // Randaom has to be changed if used in Unity
+    // Random has to be changed if used in Unity
     public void GiveRandomNumbers()
     {
         Random randomNumber = new Random();
-        for (int i = 0; i < bais.Length; i++)
+        for (int i = 0; i < bias.Length; i++)
         {
-            bais[i] = randomNumber.Next(-25, 25);
+            bias[i] = randomNumber.Next(-25, 25);
         }
 
-        for (int i = 0; i < weigths.Length; i++)
+        for (int i = 0; i < weights.Length; i++)
         {
-            for (int j = 0; j < weigths[i].Length; j++)
+            for (int j = 0; j < weights[i].Length; j++)
             {
-                for (int k = 0; k < weigths[i][j].Length; k++)
+                for (int k = 0; k < weights[i][j].Length; k++)
                 {
-                    weigths[i][j][k] = randomNumber.Next(-25, 25);
-
+                    weights[i][j][k] = randomNumber.Next(-25, 25);
                 }
             }
         }
-
     }
-
-
-
 }
