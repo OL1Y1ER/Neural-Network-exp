d0ec0e8a055ffa73ccd5ad7d9c10899aa058495b
OL1Y1ER <134446761+OL1Y1ER@users.noreply.github.com>
2023-09-26 21:47:20 +0200

    Update neuralNetwork.cs

--- neuralNetwork.cs
+++ neuralNetwork.cs
@@ -1,3 +1,11 @@
+/*-----------------------------------------------------------------
+Improved version of NeuralNetwork class
+
+
+@author Olivier de la Porte
+-----------------------------------------------------------------*/
+
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,6 +14,7 @@ using System.Threading.Tasks;
 
 
 class NeuralNetwork{
+
 
 }
 
